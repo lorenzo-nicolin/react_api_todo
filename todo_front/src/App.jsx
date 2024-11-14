5b790307dff2e009f48ce886c592f6602abb60ff
Lorenzo <lorenzogovender152@gmail.com>
2024-11-14 08:45:43 +0200

    add the post

--- todo_front/src/App.jsx
+++ todo_front/src/App.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 
 function App() {
   const [todos, setTodos] = useState([]); // Renamed todoName to todos
+  const [todosadd, setTodosadd] = useState(""); // Renamed todoName to todos
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -34,16 +35,46 @@ function App() {
 
       //  setTodos(response.data);
       console.log(response.data);
-      fetchTodos();
     } catch (error) {
       setError(error.message);
     } finally {
-      setLoading(false);
+      fetchTodos();
     }
+    setLoading(false);
+  };
+
+  const handlePost = async () => {
+    // console.log(todosadd);
+
+    setLoading(true);
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/todoitems/",
+        {
+          todosadd,
+        }
+      );
+
+      console.log(response.data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      fetchTodos();
+    }
+    setLoading(false);
   };
 
   return (
     <>
+      <div className="addform">
+        <input
+          type="text"
+          value={todosadd}
+          onChange={(event) => setTodosadd(event.target.value)}
+        />
+
+        <button onClick={handlePost}>Add</button>
+      </div>
       <div>
         <h1>Todo From Laravel</h1>
         {loading ? (
